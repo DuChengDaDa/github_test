f912953dc6d7f4de0e7b4f1afc1608b3540f90ed
dc <1617798384@qq.com>
2021-08-25 09:14:08 +0800

    这是github测试v1.0

---
+++ src/test/java/cn/baizhi/GithubTestApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("yyy is bigSB");
+    }
+
+}
